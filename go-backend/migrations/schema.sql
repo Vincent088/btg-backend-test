665630ba7b3b3c3a6bac52c03c861c38e4e29eee
vincent <vinvincent44@gmail.com>
2026-07-07 21:29:23 +0700

    feat: add unique constraint on nationality_code and seed common nationalities

--- go-backend/migrations/schema.sql
+++ go-backend/migrations/schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS nationality (
+CREATE TABLE nationality (
     nationality_id SERIAL PRIMARY KEY,
     nationality_name VARCHAR(50) NOT NULL,
-    nationality_code CHAR(2) NOT NULL
+    nationality_code CHAR(2) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS customer (
